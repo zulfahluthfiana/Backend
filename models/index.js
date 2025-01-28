176ec2597f144d22466cda54e173e86211248879
zulfahluthfiana <zulfa070702@gmail.com>
2025-01-28 16:09:30 +0700

    fix

--- models/index.js
+++ models/index.js
@@ -11,9 +11,18 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config,{
+    dialectModule: require("pg"),
+});
+
+}else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+    { dialectModule: require("pg") }
+  );
 }
 
 fs
