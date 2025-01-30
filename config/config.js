480cee1ba8aa748584f5a283491804140fbccec2
zulfahluthfiana <zulfa070702@gmail.com>
2025-01-30 11:39:11 +0700

    fixconfig

--- config/config.js
+++ config/config.js
@@ -1,33 +1,31 @@
-require('dotenv').config();  // Menggunakan dotenv untuk mengakses variabel lingkungan
-
+require("dotenv").config();
 const config = {
-  "production": {
-    "username": process.env.DB_USERNAME,  // Mengakses variabel lingkungan dengan benar
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        "require": true,
-        "rejectUnauthorized": false
-      }
-    }
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  "development": {
-    "username": process.env.DB_USERNAME || "postgres",  // Default untuk development
-    "password": process.env.DB_PASSWORD || "12345678",
-    "database": process.env.DB_NAME || "toko_bakri",
-    "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  "test": {
-    "username": process.env.DB_USERNAME || "postgres",
-    "password": process.env.DB_PASSWORD || "12345678",
-    "database": process.env.DB_NAME || "toko_bakri",
-    "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres"
-  }
 };
 
 module.exports = config;
